19225f8e11b5ae387e05c55c2d65e4cd44d5c2b7
Daniel Del Moral <dadelmo@gmail.com>
2013-12-03 16:08:27 +0100

    First test, waiting for better specs

---
+++ spec/request_log_spec.rb
@@ -0,0 +1,34 @@
+require 'tck_better_request_logs/request_log'
+
+describe TckBetterRequestLogs::RequestLog do
+
+  let(:runtime) { 1000 }
+
+  let(:payload) do
+    {
+        status: 200, format: 'html', method: 'GET', path: '/home?foo=bar', params: {
+          'controller' => 'home', 'action' => 'index', 'foo' => 'bar'
+        }, db_runtime: 0.02, view_runtime: 0.01
+    }
+  end
+
+  subject do
+    TckBetterRequestLogs::RequestLog.new payload, runtime
+  end  
+
+  describe '#log' do
+    it do 
+      subject.log.should_not be_empty
+    end
+
+    it 'contains path' do
+      subject.log.should =~ /path=\[\/home/
+    end
+
+    it 'contains format' do
+      subject.log.should =~ /format=html/
+    end
+
+  end
+
+end
