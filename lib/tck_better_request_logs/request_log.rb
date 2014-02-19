9e022bd95f06d6a5fba9357ac3b66b9516fc445d
Samuel Navas <samuel.navas@the-cocktail.com>
2014-02-19 09:19:41 +0100

    Added date and process id to log message

--- lib/tck_better_request_logs/request_log.rb
+++ lib/tck_better_request_logs/request_log.rb
@@ -10,7 +10,12 @@ module TckBetterRequestLogs
     end
 
     def log
-      "TCK Request Completed status=#{payload[:status]} path=[#{payload[:path]}] format=#{payload[:format]} (views=#{payload[:view_runtime].to_i}ms | db=#{payload[:db_runtime].to_i}ms) in #{runtime}ms"
+      "TCK Request Completed #{time} status=#{payload[:status]} path=[#{payload[:path]}] format=#{payload[:format]} (views=#{payload[:view_runtime].to_i}ms | db=#{payload[:db_runtime].to_i}ms) in #{runtime}ms PID #{Process.pid}"
+    end
+
+    private
+    def time
+      Time.now.strftime("%Y-%m-%d %H:%M:%s")
     end
   end
 
